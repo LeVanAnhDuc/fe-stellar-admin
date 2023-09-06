f21d6dc9e30856cf5722b95fe8211cf0f71c62af
TinhBQ <81704759+TinhBQ@users.noreply.github.com>
2023-09-07 00:33:39 +0700

    s

--- src/apis/index.js
+++ src/apis/index.js
@@ -1,5 +1,6 @@
 export { default as authApi } from './authApi.js';
 export { default as userApi } from './userApi.js';
-export { default as bookingRoomApi } from './bookingRoomApi.js';
 export { default as typeRoomApi } from './typeRoomApi.js';
+export { default as bookingApi } from './bookingApi.js';
 export { default as roomApi } from './roomApi.js';
+
